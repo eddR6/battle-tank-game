3a7a369a538319ecd3a00634eb3589b5d54f104e
eddr6 <jainaditya400@gmail.com>
2020-10-20 19:39:38 +0530

    Tank Controller and Tank service and Joystick setup(not working)

---
+++ Assets/Scripts/TankService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankService : MonoSingletonGeneric<TankService>
+{
+    [Header("Player Tank Object")]
+    public GameObject playerTank;
+    protected override void Awake()
+    {
+        base.Awake();
+        TankService.Instance.GetTank();
+    }
+
+    public void GetTank()
+    {
+        Instantiate(playerTank, Vector3.zero, Quaternion.identity);
+    }
+}
